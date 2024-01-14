96a266367cdab9879508462b1fdfee3cc8b62657
shifait <67757312+shifait@users.noreply.github.com>
2024-01-14 22:08:34 +0600

    Add files via upload
    This is my one.

---
+++ include/Footer.php
@@ -0,0 +1,23 @@
+
+  
+<div id="Footer">
+	<hr>
+	<p style="color: #aaa;">made by | web group 1  </p>
+	<p>Feel free to contact us. <br>&#169 group1 </p>
+	<p style="font-size: 12px; font-weight: bold;">Email: <a href="#">id@gmail.com</a></p>
+	<hr>
+</div> <!-- footer -->
+
+<style >
+	a{
+		text-decoration: none;
+		color: white;
+	}
+	a:hover{
+		text-decoration: none;
+		color: blue;
+	}
+	#Footer{
+		background-color:green;
+	}
+</style>
